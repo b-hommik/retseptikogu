9276e58d91a6b5d8749dbab0967a34f802cdfb8f
Thea Rehepapp <b-hommik@hotmail.com>
2020-01-10 19:41:54 +0200

    piltide ja lehtede genereerimine

--- Functions/database.js
+++ Functions/database.js
@@ -1,3 +1,4 @@
 var pildid=["kala", "kook", "lasanje", "munad", "pada", "pasta", "pitsa", "seened"];
+var lehed=["kala", "kook", "lasanje", "munad", "pada", "pasta", "pitsa", "seened"];
 //kui tegu pole globaalse muutujaga, siis tuleks muutuja eksportida/ importida
 //export {pildid};
--- Functions/function.js
+++ Functions/function.js
@@ -1,13 +1,20 @@
 var item=document.getElementsByClassName("col-sm-3");
+var j=0;
 window.onload=tagElements();
 //import{pildid} from "database.js";
 console.log(pildid[0]);
 
 function tagElements(){
-    for(var i =0; i<item.length;i++){
+    for(var i =0; i<pildid.length;i++){
+        var link=document.createElement("a");
         var image=document.createElement("img");
-        item[i].appendChild(image);
+        item[j].appendChild(image);
+        link.href="/Pages/"+lehed[i]+".html";
         image.src="/Files/"+pildid[i]+".jpg";
+        j+=1;
+        if(j>=4){
+            j=0;
+        }
         if((i+1)%2==0){
             image.id="paaris";
         }
@@ -15,7 +22,7 @@ function tagElements(){
             image.id="paaritu";
         }
 
-        console.log(item[i]);
+        console.log(item[j]);
     }
 }
 
