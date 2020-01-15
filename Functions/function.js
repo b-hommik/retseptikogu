446b962a30ed68f04fc3d573e9d9a4f18a757437
thea <b-hommik@hotmail.com>
2020-01-15 10:16:54 +0200

    added pages and links to pages to index

--- Functions/function.js
+++ Functions/function.js
@@ -2,15 +2,15 @@ var item=document.getElementsByClassName("col-sm-3");
 var j=0;
 window.onload=tagElements();
 //import{pildid} from "database.js";
-console.log(pildid[0]);
 
 function tagElements(){
     for(var i =0; i<pildid.length;i++){
         var link=document.createElement("a");
         var image=document.createElement("img");
+        item[j].appendChild(link);
         item[j].appendChild(image);
-        link.href="/Pages/"+lehed[i]+".html";
-        image.src="/Files/"+pildid[i]+".jpg";
+        link.href="Pages/"+lehed[i]+".html";
+        image.src="Files/"+pildid[i]+".jpg";
         j+=1;
         if(j>=4){
             j=0;
